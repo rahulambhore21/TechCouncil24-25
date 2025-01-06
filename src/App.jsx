eedeb6288adedd75f5fc03d3d916bbcc9f9cb6f0
rahulambhore21 <ambhorerahul69@gmail.com>
2025-01-07 00:54:43 +0530

    Host 01 minor

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Testing />} />
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/sponsors" element={<Sponsors />} />
         <Route path="/about" element={<About_Us />} />
