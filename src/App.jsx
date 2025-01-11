9c262c678a382915ee7a3d729da7eeb4f1476d2a
rahulambhore21 <ambhorerahul69@gmail.com>
2025-01-11 23:28:31 +0530

    final host 01 --minor 09 rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,21 @@ import About_Us from "./page/About_Us";
 import Sponsors from "./page/Sponsors";
 import Contact from "./page/Contact_Us";
 import Home from "./page/Home";
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom"; // Updated import
 import Events from "./page/Events";
 import Notfound from "./page/Notfound";
 
-
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/sponsors" element={<Sponsors />} />
-        <Route path="/about" element={<About_Us />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="*" element={<Notfound/>} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/sponsors" element={<Sponsors />} />
+      <Route path="/about" element={<About_Us />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="*" element={<Notfound/>} />
+    </Routes>
   );
 }
 
